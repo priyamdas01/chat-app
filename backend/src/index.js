2006f56c5ef82d2b8b9536d418bfafb4460649f2
priyamdas01 <12priyamdas34@gmail.com>
2025-07-11 00:22:21 -0400

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-
+import { connectDB } from "./lib/db.js";
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.port || 5001;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+  connectDB();
 });
---
+++ backend/src/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: false }
+);
+
+const message = mongoose.model("Message", messageSchema);
+
+export default message;
